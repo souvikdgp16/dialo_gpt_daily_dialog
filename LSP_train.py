98a35b5d421789a46044f0a877db1e93a3e89b31
souvikdgp16 <souvik.nitd15@gmail.com>
2020-07-11 14:12:50 -0400

    dataloader changes

--- LSP_train.py
+++ LSP_train.py
@@ -192,8 +192,8 @@ else:
 eval_dataloader_loss = BucketingDataLoader(args.eval_input_file, 
         args.eval_batch_size, args.max_seq_length)
 
-eval_dataloader_gen = get_eval_list_same_length(
-    args.eval_input_file, enc, args.eval_batch_size, True)
+# eval_dataloader_gen = get_eval_list_same_length(
+#     args.eval_input_file, enc, args.eval_batch_size, True)
 
 
 #########################################################################
