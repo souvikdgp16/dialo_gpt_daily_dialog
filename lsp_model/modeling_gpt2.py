660cf42a0700d944b8228e13635c4c7822689813
souvikdgp16 <souvik.nitd15@gmail.com>
2020-07-12 01:08:28 -0400

    dataloader changes

--- lsp_model/modeling_gpt2.py
+++ lsp_model/modeling_gpt2.py
@@ -109,21 +109,21 @@ class GPT2LMHeadModel(GPT2PreTrainedModel):
                 emotion_logits = self.emotion_head(hidden_states[:, -1, :])
                 #print(hidden_states.shape, emotion_logits.shape)
                 
-                loss_fct_emotion = torch.nn.CrossEntropyLoss(ignore_index=-1, reduction='none')
+                loss_fct_emotion = torch.nn.CrossEntropyLoss()
                 loss_emotion = loss_fct_emotion(emotion_logits.view(-1, 7), emotion_labels.view(-1))
                 #loss_emotion = loss_fct_emotion(emotion_logits.view(-1, 7), emotion_labels.view(-1))
 
-                loss += 0.1*loss_emotion
+                #loss += 0.1*loss_emotion
 
             if da_labels is not None:
                 da_logits = self.da_head(hidden_states[:, -1, :])
                 #print(hidden_states.shape, emotion_logits.shape)
                 
-                loss_fct_da = torch.nn.CrossEntropyLoss(ignore_index=-1, reduction='none')
+                loss_fct_da = torch.nn.CrossEntropyLoss()
                 loss_da = loss_fct_da(da_logits.view(-1, 4), da_labels.view(-1))
                 #loss_emotion = loss_fct_emotion(emotion_logits.view(-1, 7), emotion_labels.view(-1))
 
-                loss += 0.1*loss_da
+                #loss += 0.1*loss_da
 
 
 
